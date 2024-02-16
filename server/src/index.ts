3dcd4bd568200fa2c13df73fa3930eff9ac869ac
aswanth6000 <aswanth682@gmail.com>
2024-02-16 07:24:11 +0530

    router added

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import connectToDatabase from './config/DBConfig';
 import router from './modules/routes/auth-routes';
+import fileRouter from './modules/routes/file-routes';
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use(express.json())
 
 //Routers
 app.use(router)
+app.use(fileRouter)
 
 
 //database connection 
